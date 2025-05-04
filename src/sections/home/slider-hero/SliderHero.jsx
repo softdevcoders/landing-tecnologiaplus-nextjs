15bc1d3934d51b0ebdb695425a3afa73cf0ed79e
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-05-04 09:33:26 -0300

    wip

--- src/sections/home/slider-hero/SliderHero.jsx
+++ src/sections/home/slider-hero/SliderHero.jsx
@@ -1,10 +1,24 @@
 import styles from "./SliderHero.module.scss";
 import { SLIDER_HERO_ITEMS } from "./slides";
 import SliderHeroClient from "./SliderHeroClient";
+import ImageCloudinary from "@/components/ui/image-cloudinary";
 
 export default function SliderHero() {
   return (
     <div className={styles.slider__container}>
+       <ImageCloudinary
+        src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349921/jlxxsnlercoj0nihz4am_udppso.webp"
+        alt="Background Image for Slider Hero"
+        fill
+        priority
+        fetchPriority="high"
+        sizes="(min-width: 1024px) 25vw, 100px"
+        style={{
+          objectFit: 'cover',
+          objectPosition: 'center',
+          zIndex: 0,
+        }}
+      />
       <SliderHeroClient items={SLIDER_HERO_ITEMS} styles={styles} />
     </div>
   );
