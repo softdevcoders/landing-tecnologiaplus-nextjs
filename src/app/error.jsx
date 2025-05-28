5368b719d02ec21ad58cc72a8ac514e8ff1f8528
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-05-28 12:56:46 -0300

    wip

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,23 +1,25 @@
+'use client';
+
 import Link from 'next/link';
-import styles from './not-found.module.scss';
+import styles from './error.module.scss';
 import Header from '@/components/layout/header/Header';
 
 export const metadata = {
-  title: 'Página no encontrada | Tecnología Plus',
-  description: 'Lo sentimos, la página que estás buscando no existe o ha sido movida.',
+  title: 'Error 500 | Tecnología Plus',
+  description: 'Lo sentimos, ha ocurrido un error en el servidor.',
   robots: {
     index: false,
     follow: true,
   },
 };
 
-export default function NotFound() {
+export default function Error() {
   return (
     <>
       <Header headerAlt={true} />
       <div className={styles.container}>
-        <h1>404 - Página no encontrada</h1>
-        <p>Lo sentimos, la página que estás buscando no existe o ha sido movida.</p>
+        <h1>Error 500 - Ha ocurrido un error en el servidor</h1>
+        <p>Lo sentimos, ha ocurrido un error en el servidor. Por favor, intenta nuevamente más tarde.</p>
         <Link href="/" className={styles.button}>
           Volver a la página de inicio
         </Link>
