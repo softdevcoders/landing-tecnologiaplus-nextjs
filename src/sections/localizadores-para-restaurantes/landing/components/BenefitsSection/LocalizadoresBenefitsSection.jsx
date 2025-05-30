5e4940630511d8e55f2881025b483e2e65f82d67
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-05-30 16:00:51 -0300

    loc, prod sec

--- src/sections/localizadores-para-restaurantes/landing/components/BenefitsSection/LocalizadoresBenefitsSection.jsx
+++ src/sections/localizadores-para-restaurantes/landing/components/BenefitsSection/LocalizadoresBenefitsSection.jsx
@@ -3,46 +3,8 @@ import Image from "next/image";
 import style from "./LocalizadoresBenefitsSection.module.scss";
 
 function LocalizadoresBenefitsSection() {
-  // return (
-  //   <section className={style.benefits__section}>
-  //     <h2 className={style.benefits__title}>
-  //       BENEFICIOS
-  //       <br />
-  //       DESTACADOS
-  //     </h2>
-  //     <ul>
-  //       <li>
-  //         <img
-  //           loading="lazy"
-  //           src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738431751/icono_1_2x-8_zw5neg.webp"
-  //           alt="icono de sonido"
-  //         />
-  //         Vibran, suenan y alumbran.
-  //       </li>
-  //       <li>
-  //         <img
-  //           loading="lazy"
-  //           src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738431754/icono_2_2x-8_vktxnl.webp"
-  //           alt="icono de conectividad y largo alcance"
-  //         />
-  //         Largo Alcance, más de
-  //         <br />
-  //         200 metros a la redonda.
-  //       </li>
-  //       <li>
-  //         <img
-  //           loading="lazy"
-  //           src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738851894/icono_3_2x-8_1_nftdzj.webp"
-  //           alt="icono de medalla de excelencia"
-  //         />
-  //         Material resistente
-  //         <br />a golpes y caídas.
-  //       </li>
-  //     </ul>
-  //   </section>
-  // );
   return (
-    <BenefitsSection>
+    <BenefitsSection overlayWhite>
       <div className={style.benefits__container}>
         <div className={style.text__container}>
           <div className={style.benefits__title_container}>
--- src/sections/localizadores-para-restaurantes/landing/components/SecondaryProductsSection/LocalizadoresSecondaryProductsSection.jsx
+++ src/sections/localizadores-para-restaurantes/landing/components/SecondaryProductsSection/LocalizadoresSecondaryProductsSection.jsx
@@ -7,68 +7,73 @@ import Image from "next/image";
 function LocalizadoresSecondaryProductsSection() {
   return (
     <>
-      <section className={`${style.products__section}  ${style.bg__section}`}>
-        <div className={style.products__container}>
+      <section className={style.secondary__products__section}>
+        <div className={style.secondary__products__container}>
           <div
-            className={`${style.products__card__container} ${style.products__card__container__variant}`}
+            className={style.secondary__products__card__container}
           >
-            <div className={style.card}>
-              <div>
-                <h3>REC V3</h3>
+            <div className={style.secondary__products__card}>
+              <div className={style.secondary__products__card__content}>
+                <h3 className={style.secondary__products__card__title}>REC V3</h3>
                 <Link
-                  className={style.card__btn}
+                  className={style.secondary__products__card__btn}
                   href={routes.landings.localizadoresParaRestaurantes.children.rec_v3.url}
                 >
                   Ver más
                 </Link>
-                <p>2.595 vendidos</p>
+                <p className={style.secondary__products__card__text}>2.595 vendidos</p>
               </div>
+              <div className={style.secondary__products__card__image__container}>
+                <Image
+                  width={800}
+                  height={595}
+                  src="https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale/w_800/v1738855044/rec_v3_1_2x-8_ctzrqp.webp"
+                  alt="Localizador para restaurantes REC V3 junto a su base de carga"
+                />
+              </div>
+            </div>
+            <div className={style.secondary__products__separator__container}>
               <Image
-                width={800}
-                height={595}
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale/w_800/v1738855044/rec_v3_1_2x-8_ctzrqp.webp"
-                alt="Localizador para restaurantes REC V3 junto a su base de carga"
+                width={5}
+                height={350}
+                className={style.secondary__products__separator}
+                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale/w_5/v1738851946/linea_2x-8_n80wv9.webp"
+                alt="Separador"
               />
             </div>
-            <Image
-              width={5}
-              height={350}
-              className={style.separator}
-              src="https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale/w_5/v1738851946/linea_2x-8_n80wv9.webp"
-              alt="Separador"
-            />
-            <div className={style.card}>
-              <div>
-                <h3>CIR C2</h3>
+            <div className={style.secondary__products__card}>
+              <div className={style.secondary__products__card__content}>
+                <h3 className={style.secondary__products__card__title}>CIR C2</h3>
                 <Link
-                  className={style.card__btn}
+                  className={style.secondary__products__card__btn}
                   href={routes.landings.localizadoresParaRestaurantes.children.rec_c2.url}
                 >
                   Ver más
                 </Link>
-                <p>1.630 vendidos</p>
+                <p className={style.secondary__products__card__text}>1.630 vendidos</p>
               </div>
-              <Image
-                width={800}
-                height={595}
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale/w_800/v1738851948/cir_c2_1_2x-8_lb8ufb.webp"
-                alt="Localizadores para clientes CIR C2 junto a su base de carga"
-              />
+              <div className={style.secondary__products__card__image__container}>
+                <Image
+                  width={800}
+                  height={595}
+                  src="https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale/w_800/v1738851948/cir_c2_1_2x-8_lb8ufb.webp"
+                  alt="Localizadores para clientes CIR C2 junto a su base de carga"
+                />
+              </div>
             </div>
           </div>
-          <p>
-            Localizador de clientes con señal de largo alcance y material
-            resistente a golpes o caídas.
+          <p className={style.secondary__products__description}>
+            Localizador de clientes con señal de largo alcance y material resistente a golpes o caídas.
           </p>
         </div>
       </section>
-      <section className={`${style.products__section} ${style.video__section}`}>
-        <div className={style.products__container}>
+      <section className={style.video__section}>
+        <div className={style.video__container}>
           <h2>Veamos funcionar nuestros Localizadores para restaurantes</h2>
           <div
-            className={`${style.products__card__container} ${style.video__container}`}
+            className={style.video__card__container}
           >
-            <div className={style.card__video}>
+            <div className={style.video__card}>
               <p>Rec V3 - Localizadores para restaurantes</p>
               <YouTubePlayer
                 videoId="aB7FLAPCCJ8"
@@ -77,7 +82,7 @@ function LocalizadoresSecondaryProductsSection() {
               />
             </div>
 
-            <div className={style.card__video}>
+            <div className={style.video__card}>
               <p>Cir C2 - Localizadores para restaurantes</p>
               <YouTubePlayer
                 videoId="b2nlu7sOlcI"
