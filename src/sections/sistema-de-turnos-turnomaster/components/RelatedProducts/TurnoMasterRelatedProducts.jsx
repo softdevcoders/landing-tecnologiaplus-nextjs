6942d31c29c8b15788deb21277787f85cfa2f4b5
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-06-04 14:48:32 -0300

    Sistemas de turnos

--- src/sections/sistema-de-turnos-turnomaster/components/RelatedProducts/TurnoMasterRelatedProducts.jsx
+++ src/sections/sistema-de-turnos-turnomaster/components/RelatedProducts/TurnoMasterRelatedProducts.jsx
@@ -1,8 +1,33 @@
 import RelatedProducts from "@/components/related-products";
+import { routes } from "@/config/routes";
 
 function TurnoMasterRelatedProducts() {
+
+  const products = [
+    {
+      name: "Rollos térmicos",
+      description: "Perfectos para tickets, facturas y más.",
+      img: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349920/image-1_1_ljyxis_qdsmdq.webp",
+      sold: "14.485 vendidos",
+      link: routes.landings.rollosDePapelTermico.url,
+    },
+    {
+      name: "TurnoExpress",
+      description: "Rapidez y agilidad para tus filas.",
+      img: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738350028/TurnoExpress_2x-8_ri7zqh_xjz8bw.webp",
+      sold: "692 vendidos",
+      link: routes.landings.turneroTurnoexpress.url,
+    },
+    {
+      name: "Rollos de turnos",
+      description: "Organiza y mejora el servicio.",
+      img: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1745436638/Frame_1_35_vku1gs_746c0e.webp",
+      sold: "8.527 vendidos",
+      link: routes.landings.rollosDeFichosParaTurnos.url,
+    },
+  ];
   return (
-    <RelatedProducts />
+    <RelatedProducts products={products} />
   )
 }
 
