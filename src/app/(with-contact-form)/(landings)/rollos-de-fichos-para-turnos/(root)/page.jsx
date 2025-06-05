ebd5dd6aa19332b9e46072ffcf0b9866b2f51805
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-06-05 14:22:07 -0300

    Fix blog image

--- src/app/(with-contact-form)/(landings)/rollos-de-fichos-para-turnos/(root)/page.jsx
+++ src/app/(with-contact-form)/(landings)/rollos-de-fichos-para-turnos/(root)/page.jsx
@@ -1,48 +1,26 @@
-// Importación de componentes específicos de la página
 import getMetadata from "@/request/server/metadata/get-metadata";
-import TurnosBenefitsSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/BenefitsSection/TurnosBenefitsSection"; // Sección de beneficios
-import TurnosClientsSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/ClientsSection/TurnosClientsSection"; // Sección de clientes
-import TurnosHeroSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/HeroSection/TurnosHeroSection"; // Sección principal (hero)
-import TurnoIndustriesSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/IndustriesSection/TurnoIndustriesSection"; // Sección de industrias
-import TurnosProductsSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/ProductsSection/TurnosProductsSection"; // Sección de productos
-import TurnosRelatedProducts from "@/sections/rollos-de-fichos-para-turnos/landing/components/RelatedProducts/TurnosRelatedProducts"; // Sección de productos relacionados
-import TurnosTechnicalDetails from "@/sections/rollos-de-fichos-para-turnos/landing/components/TechnicalDetails/TurnosTechnicalDetails"; // Sección de detalles técnicos
+import TurnosBenefitsSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/BenefitsSection/TurnosBenefitsSection";
+import TurnosClientsSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/ClientsSection/TurnosClientsSection";
+import TurnosHeroSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/HeroSection/TurnosHeroSection";
+import TurnoIndustriesSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/IndustriesSection/TurnoIndustriesSection";
+import TurnosProductsSection from "@/sections/rollos-de-fichos-para-turnos/landing/components/ProductsSection/TurnosProductsSection";
+import TurnosRelatedProducts from "@/sections/rollos-de-fichos-para-turnos/landing/components/RelatedProducts/TurnosRelatedProducts";
+import TurnosTechnicalDetails from "@/sections/rollos-de-fichos-para-turnos/landing/components/TechnicalDetails/TurnosTechnicalDetails";
 
 export async function generateMetadata() {
   const { 'rollos-de-fichos-para-turnos': { root: metadata } } = getMetadata('landings');
   return metadata;
 }
 
-// Componente principal de la página
 export default function RollosTurnosLanding() {
 	return (
 		<>
-			{/* Sección principal (hero) */}
-			{/* Props esperados: Ninguno */}
 			<TurnosHeroSection />
-
-			{/* Sección de clientes */}
-			{/* Props esperados: Ninguno */}
 			<TurnosClientsSection />
-
-			{/* Sección de productos */}
-			{/* Props esperados: Ninguno */}
 			<TurnosProductsSection />
-
-			{/* Sección de beneficios */}
-			{/* Props esperados: Ninguno */}
 			<TurnosBenefitsSection />
-
-			{/* Sección de detalles técnicos */}
-			{/* Props esperados: Ninguno */}
 			<TurnosTechnicalDetails />
-
-			{/* Sección de industrias */}
-			{/* Props esperados: Ninguno */}
 			<TurnoIndustriesSection />
-
-			{/* Sección de productos relacionados */}
-			{/* Props esperados: Ninguno */}
 			<TurnosRelatedProducts />
 		</>
 	);
