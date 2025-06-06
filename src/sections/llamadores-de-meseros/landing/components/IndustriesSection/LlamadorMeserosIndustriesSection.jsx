03394570425f772fb994472f552c5037e8575046
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-06-05 21:38:19 -0300

    Industries

--- src/sections/llamadores-de-meseros/landing/components/IndustriesSection/LlamadorMeserosIndustriesSection.jsx
+++ src/sections/llamadores-de-meseros/landing/components/IndustriesSection/LlamadorMeserosIndustriesSection.jsx
@@ -1,67 +1,53 @@
-import style from "./LlamadorMeserosIndustriesSection.module.scss";
-import Image from "next/image";
+import { 
+  IndustriesSection, 
+  IndustriesSectionContainer,
+  IndustriesSectionTitle, 
+  IndustriesSectionList, 
+  IndustriesSectionListItem, 
+  IndustriesSectionListItemIcon, 
+  IndustriesSectionListItemIconContainer,
+  IndustriesSectionListItemText 
+} from "@/components/industries-section";
 
 function LlamadorMeserosIndustriesSection() {
+  const industries = [
+    {
+      icon: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738694400/restaurantes_2x-8_lpqdzj.webp",
+      alt: "icono de Restaurantes",
+      name: "Restaurantes",
+    },
+    {
+      icon: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738694403/hoteles_2x-8_odgal8.webp",
+      alt: "icono de Hoteles",
+      name: "Hoteles",
+    },
+    {
+      icon: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738694400/otras_industrias_2x-8_jwvjrm.webp",
+      alt: "icono de Otras industrias",
+      name: "Otras industrias",  
+    },
+  ];
+
   return (
-    <section className={style.industries__section}>
-      <div className={style.industries__section__background}>
-        <Image
-          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349923/nisgh2gbe0ginddnsrko_opkrn1.webp"
-          alt="background de la sección de industrias"
-          width={500}
-          height={500}
-          className={style.industries__section__background__image}
-        />
-      </div>
-      <div className={style.industries__section__container}>
-        <h2 className={style.industries__section__title}>
-          LLAMADOR DE PERSONAL
-          <br />
-          ADAPTABLE A TODOS
-          <br />
-          LOS SECTORES
-        </h2>
-        <ul className={style.industries__section__list}>
-          <li className={style.industries__section__list__item}>
-            <div className={style.industries__section__list__item__icon_container}>
-              <Image
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738694400/restaurantes_2x-8_lpqdzj.webp"
-                alt="icono de Restaurantes"
-                width={500}
-                height={500}
-              />
-            </div>
-            <p className={style.industries__section__list__item__text}>
-              Restaurantes
-            </p>
-          </li>
-          <li className={style.industries__section__list__item}>
-            <div className={style.industries__section__list__item__icon_container}>
-              <Image
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738694403/hoteles_2x-8_odgal8.webp"
-                alt="icono de Sector salud"
-                width={500}
-                height={500}
-              />
-            </div>
-            <p className={style.industries__section__list__item__text}>Hoteles</p>
-          </li>
-          <li className={style.industries__section__list__item}>
-            <div className={style.industries__section__list__item__icon_container}>
-              <Image
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738615995/otras_industrias_2x-8_jwvjrm.webp"
-                alt="icono de otras industrias"
-                width={500}
-                height={500}
-              />
-            </div>
-            <p className={style.industries__section__list__item__text}>Otras industrias</p>
-          </li>
-        </ul>
-      </div>
-    </section>
+    <IndustriesSection>
+      <IndustriesSectionContainer>
+          <IndustriesSectionTitle title="LLAMADOR DE PERSONAL ADAPTABLE A TODOS LOS SECTORES" />
+          <IndustriesSectionList> 
+          {industries.map((industry, index) => (
+            <IndustriesSectionListItem key={index}>
+              <IndustriesSectionListItemIconContainer>
+                <IndustriesSectionListItemIcon src={industry.icon} alt={industry.alt} />
+              </IndustriesSectionListItemIconContainer>
+              <IndustriesSectionListItemText text={industry.name} />
+            </IndustriesSectionListItem>
+          ))}
+        </IndustriesSectionList>
+      </IndustriesSectionContainer>
+    </IndustriesSection>
   );
 }
 
 export default LlamadorMeserosIndustriesSection;
+
+
 
--- src/sections/turnero-turnoexpress/root/components/IndustriesSection/TurnoExpressIndustriesSection.jsx
+++ src/sections/turnero-turnoexpress/root/components/IndustriesSection/TurnoExpressIndustriesSection.jsx
@@ -1,65 +1,50 @@
-import Image from "next/image";
-import style from "./TurnoExpressIndustriesSection.module.scss";
+import { 
+  IndustriesSection, 
+  IndustriesSectionContainer,
+  IndustriesSectionTitle, 
+  IndustriesSectionList, 
+  IndustriesSectionListItem, 
+  IndustriesSectionListItemIcon, 
+  IndustriesSectionListItemIconContainer,
+  IndustriesSectionListItemText 
+} from "@/components/industries-section";
 
 function TurnoExpressIndustriesSection() {
+  const industries = [
+    {
+      icon: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738350027/Salud_2x-8_vg4jxo_dyuifd.webp",
+      alt: "icono de Sector salud",
+      name: "Sector salud", 
+    },
+    {
+      icon: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738446073/Sala_de_espera_2x-8_vb2upo.webp",
+      alt: "icono de Sala de espera",
+      name: "Sala de espera",  
+    },
+    {
+      icon: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349970/otras_indistrias_2x-8_bvq8fo_s2xoyg.webp",   
+      alt: "icono de Otras industrias", 
+      name: "Otras industrias",  
+    },
+  ];
+
   return (
-    <section className={style.industries__section}>
-      <div className={style.industries__section__background}>
-        <Image
-          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349923/nisgh2gbe0ginddnsrko_opkrn1.webp"
-          alt="background de la sección de industrias"
-          width={500}
-          height={500}
-          className={style.industries__section__background__image}
-        />
-      </div>
-      <div className={style.industries__section__container}>
-        <h2 className={style.industries__section__title}>
-          Nuestro Turnero Digital se adapta a Todos los sectores
-        </h2>
-        <ul className={style.industries__section__list}>
-          <li className={style.industries__section__list__item}>
-            <div className={style.industries__section__list__item__icon_container}>
-              <Image
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738350027/Salud_2x-8_vg4jxo_dyuifd.webp"
-                alt="icono de salud"
-                width={500}
-                height={500}
-              />
-            </div>
-            <p className={style.industries__section__list__item__text}>
-              Sector salud
-            </p>
-          </li>
-          <li className={style.industries__section__list__item}>
-            <div className={style.industries__section__list__item__icon_container}>
-              <Image
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738446073/Sala_de_espera_2x-8_vb2upo.webp"
-                alt="icono de sala de espera"
-                width={500}
-                height={500}
-              />
-            </div>
-            <p className={style.industries__section__list__item__text}>Sala de espera</p>
-          </li>
-          <li className={style.industries__section__list__item}>
-            <div className={style.industries__section__list__item__icon_container}>
-              <Image
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349970/otras_indistrias_2x-8_bvq8fo_s2xoyg.webp"
-                alt="icono de otras industrias"
-                width={500}
-                height={500}
-              />
-            </div>
-            <p className={style.industries__section__list__item__text}>Otras industrias</p>
-          </li>
-        </ul>
-      </div>
-    </section>
+    <IndustriesSection>
+      <IndustriesSectionContainer>
+          <IndustriesSectionTitle title="Nuestro Turnero Digital se adapta a Todos los sectores" />
+          <IndustriesSectionList> 
+          {industries.map((industry, index) => (
+            <IndustriesSectionListItem key={index}>
+              <IndustriesSectionListItemIconContainer>
+                <IndustriesSectionListItemIcon src={industry.icon} alt={industry.alt} />
+              </IndustriesSectionListItemIconContainer>
+              <IndustriesSectionListItemText text={industry.name} />
+            </IndustriesSectionListItem>
+          ))}
+        </IndustriesSectionList>
+      </IndustriesSectionContainer>
+    </IndustriesSection>
   );
 }
 
 export default TurnoExpressIndustriesSection;
-
-
-
