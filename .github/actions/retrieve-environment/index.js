9e72ab3579513048e1b3271f2de1cf2028a68497
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-06-07 22:19:07 -0300

    wip

--- .github/actions/retrieve-environment/index.js
+++ .github/actions/retrieve-environment/index.js
@@ -4,10 +4,11 @@ const github = require('@actions/github');
 try {
   const eventType = github.context.eventName;
   let environmentName;
+  let currentBranch;
 
   switch (eventType) {
     case 'push':
-      const currentBranch = github.context.payload.ref.replace('refs/heads/', '');
+      currentBranch = github.context.payload.ref.replace('refs/heads/', '');
       if (currentBranch === 'main') {
         environmentName = 'production';
       } else if (currentBranch === 'development') {
@@ -18,7 +19,15 @@ try {
       }
       break;
     case 'workflow_dispatch':
-      environmentName = 'production';
+      currentBranch = github.context.payload.ref.replace('refs/heads/', '');  
+      if (currentBranch === 'main') {
+        environmentName = 'production';
+      } else if (currentBranch === 'development') {
+        environmentName = 'development';
+      } else {
+        core.setFailed(`Unsupported branch: ${currentBranch}`);
+        return;
+      }
       break;
     default:
       core.setFailed(`Unsupported event type: ${eventType}`);
