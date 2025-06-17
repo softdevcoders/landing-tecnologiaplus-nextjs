e43e91071262b4fd26ccf13da519506b60ec07c3
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-06-17 15:25:20 -0300

    Image

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
         pathname: '**',
       }
     ],
-    unoptimized: process.env.NODE_ENV === 'production',
+    // unoptimized: process.env.NODE_ENV === 'production',
     deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60 * 60 * 24 * 7,
