1f7eccd6617e8c404f8fa20be040c941ddaa6fec
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-06-23 12:45:44 -0300

    Correccion de dimesion de imagenes

--- src/app/(landings)/sistema-de-turnos-turnomaster/page.jsx
+++ src/app/(landings)/sistema-de-turnos-turnomaster/page.jsx
@@ -21,7 +21,7 @@ export default function TurnoMasterLanding() {
       <TurnoMasterBenefitsSection />
       <SecondaryTurnoMasterProductsSection />
       <TurnoMasterIndustriesSection />
-      <TurnoMasterRelatedProducts />
+      {/* <TurnoMasterRelatedProducts /> */}
     </>
   );
 }
