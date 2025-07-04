7ae25ca7e6ede9a736f82d6ecd440692ce482ae9
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-04 14:19:57 -0300

    cambios

--- src/app/(landings)/localizadores-para-restaurantes/(root)/page.jsx
+++ src/app/(landings)/localizadores-para-restaurantes/(root)/page.jsx
@@ -6,7 +6,7 @@ import LocalizadoresIndustriesSection from "@/sections/localizadores-para-restau
 import LocalizadoresProductsSection from "@/sections/localizadores-para-restaurantes/landing/components/ProductsSection/LocalizadoresProductsSection";
 import LocalizadoresRelatedProducts from "@/sections/localizadores-para-restaurantes/landing/components/RelatedProducts/LocalizadoresRelatedProducts";
 import LocalizadoresSecondaryProductsSection from "@/sections/localizadores-para-restaurantes/landing/components/SecondaryProductsSection/LocalizadoresSecondaryProductsSection";
-import LocalizadoresPreguntasFrecuentes from "@/sections/localizadores-para-restaurantes/landing/components/PreguntasFrecuentes";
+// import LocalizadoresPreguntasFrecuentes from "@/sections/localizadores-para-restaurantes/landing/components/PreguntasFrecuentes";
 export async function generateMetadata() {
   const { 'localizadores-para-restaurantes': { root: metadata } } = getMetadata('landings');
   return metadata;
@@ -21,7 +21,7 @@ export default function LocalizadoresLanding() {
       <LocalizadoresBenefitsSection />
       <LocalizadoresSecondaryProductsSection />
       <LocalizadoresIndustriesSection />
-      <LocalizadoresPreguntasFrecuentes />
+      {/* <LocalizadoresPreguntasFrecuentes /> */}
       <LocalizadoresRelatedProducts />
     </>
   );
