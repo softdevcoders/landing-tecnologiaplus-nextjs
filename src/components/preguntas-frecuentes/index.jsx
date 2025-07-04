9261e3156d6ddd820f4bf69d1832c001254f9ec5
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-03 23:25:22 -0300

    avance

--- src/components/preguntas-frecuentes/index.jsx
+++ src/components/preguntas-frecuentes/index.jsx
@@ -1,34 +1,44 @@
 'use client';
-import { useState } from 'react';
 
+import { useState } from 'react';
 import styles from './preguntas-frecuentes.module.scss';
-// import Accordion from '@/components/ui/accordion';  
 
 function PreguntasFrecuentes({ preguntasFrecuentes = [] }) {
-
   const [openAccordion, setOpenAccordion] = useState(null);
+  const [showAllQuestions, setShowAllQuestions] = useState(false);
+
+  const initialQuestionsCount = 5;
+  const hasMoreQuestions = preguntasFrecuentes.length > initialQuestionsCount;
 
   return (
-    <section className={styles.faq__section}>
-      <h2 className={styles.faq__title}>Preguntas Frecuentes</h2>
-      <div className={styles.faq__list}>
+    <section className={styles.s}>
+      <h2>Preguntas Frecuentes</h2>
+      <div data-show={showAllQuestions}>
         {preguntasFrecuentes.map((faq, index) => (
-          <div className={styles.accordion} key={index}>
+          <div 
+            key={index}
+            data-hidden={index >= initialQuestionsCount}
+          >
             <button
-              className={`${styles.accordion__header} ${index === openAccordion ? styles.open : ''}`}
+              data-open={index === openAccordion}
               onClick={() => {
                 setOpenAccordion(openAccordion === index ? null : index);
               }}
             >
-              <h3 className={styles.accordion__title}>{faq.question}</h3>
-              <span className={styles.accordion__icon}>{openAccordion === index ? '-' : '+'}</span>
+              <h3>{faq.question}</h3>
+              <span>{openAccordion === index ? '-' : '+'}</span>
             </button>
-              <div className={`${styles.accordion__content} ${openAccordion === index ? styles.open : ''}`}>
+            <div data-open={index === openAccordion}>
               {faq.answer}
             </div>
           </div>
         ))}
       </div>
+      {hasMoreQuestions && (
+        <button onClick={() => setShowAllQuestions(!showAllQuestions)}>
+          {showAllQuestions ? 'Ver menos preguntas' : `Ver ${preguntasFrecuentes.length - initialQuestionsCount} preguntas más`}
+        </button>
+      )}
     </section>
   );
 }
