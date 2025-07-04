854211303915f4080c1e76fb4b1ad3bce628ba6f
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-04 14:01:43 -0300

    avances

--- src/components/related-products/product-card.jsx
+++ src/components/related-products/product-card.jsx
@@ -1,28 +1,30 @@
 import Link from 'next/link';
-import style from './related-products.module.scss';
+import Image from 'next/image';
 
 export default function ProductCard({ product }) {
   return (
-    <article>
-      <Link href={product.link} className={style.product__link}>
-        <h3 itemProp="name">{product.name}</h3>
-        <img
-          width={320} 
-          height={230}
-          src={product.img}
-          alt={`imagen de ${product.name}`}
-          className={`${style.product__img} ${product.customClass ? product.customClass : ""}`}
-          loading="lazy"
-          itemProp="image"
-        />
-        <p itemProp="description">
-          {product.description}
-        </p>
-        <p itemProp="orderQuantity">
-          {product.sold}
-        </p>
-      </Link>
+    <>
+      <article>
+        <Link href={product.link}>
+          <h3 itemProp="name">{product.name}</h3>
+          <p itemProp="description">
+            {product.description}
+          </p>
+          <Image
+            width={320} 
+            height={230}
+            src={product.img}
+            alt={`imagen de ${product.name}`}
+            className={product.customClass ? product.customClass : ""}
+            loading="lazy"
+            itemProp="image"
+          />
+          <p itemProp="orderQuantity">
+            {product.sold}
+          </p>
+        </Link>
+      </article>
       <Link href={product.link}>Ver más</Link>
-    </article>
+    </>
   );
 } 
