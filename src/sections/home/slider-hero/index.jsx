1ba232d7652a40909d44bc1844d94a648bb58f47
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-06 19:48:18 -0300

    cambios - home

--- src/sections/home/slider-hero/index.jsx
+++ src/sections/home/slider-hero/index.jsx
@@ -12,11 +12,11 @@ export default function SliderHero() {
     <div className={styles.slider__container}>
       <Image
         fill
-        sizes="(max-width: 768px) 100vw, (max-width: 1280px) 100vw, 1600px"
-        src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_auto,q_auto/${landingHeros.background.images.src}`}
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 100vw, (max-width: 1280px) 100vw, (max-width: 1600px) 100vw, (max-width: 1920px) 100vw, 2560px"
+        src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_auto,q_auto,dpr_auto,w_auto/${landingHeros.background.images.src}`}
         alt={landingHeros.background.images.alt}
         priority
-        quality={70}
+        quality={85}
         className={styles.slider__background}
         loading="eager"
       />
