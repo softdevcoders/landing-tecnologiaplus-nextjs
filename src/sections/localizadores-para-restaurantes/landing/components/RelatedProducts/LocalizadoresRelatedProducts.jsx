114a4f8d3b88301fe942260059e237cab260eab9
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-08 22:11:44 -0300

    probando nuevo pipeline - 2

--- src/sections/localizadores-para-restaurantes/landing/components/RelatedProducts/LocalizadoresRelatedProducts.jsx
+++ src/sections/localizadores-para-restaurantes/landing/components/RelatedProducts/LocalizadoresRelatedProducts.jsx
@@ -3,12 +3,13 @@ import { categories } from "@/config/categories";
 
 function LocalizadoresRelatedProducts() {
   return (
-    <RelatedProducts productsKeys={[
-      categories.LLAMADORES_DE_MESEROS.category_key,
-      categories.TURNERO_TURNOEXPRESS.category_key,
-      categories.CALIFICADOR_DE_SERVICIO_AL_CLIENTE_OPINAMASTER.category_key,
-    ]} 
-    addMarginBottom={false}
+    <RelatedProducts 
+      productsKeys={[
+        categories.LLAMADORES_DE_MESEROS.category_key,
+        categories.TURNERO_TURNOEXPRESS.category_key,
+        categories.CALIFICADOR_DE_SERVICIO_AL_CLIENTE_OPINAMASTER.category_key,
+      ]} 
+      // addMarginBottom={false}
     />
   )
 }
