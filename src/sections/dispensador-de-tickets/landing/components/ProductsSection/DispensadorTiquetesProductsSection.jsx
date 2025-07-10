8082ce9e6f4a95efe4a6610b41459c079707131a
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-10 11:54:13 -0300

    avances-2

--- src/sections/dispensador-de-tickets/landing/components/ProductsSection/DispensadorTiquetesProductsSection.jsx
+++ src/sections/dispensador-de-tickets/landing/components/ProductsSection/DispensadorTiquetesProductsSection.jsx
@@ -2,22 +2,20 @@ import { routes } from "@/config/routes";
 import style from "./DispensadorTiquetesProductsSection.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import ToggleTextContent from "@/components/toggle-text-content";
 
 function DispensadorTiquetesProductsSection() {
   return (
     <section className={style.products__section}>
-      <Link href={routes.landings.dispensadorDeTickets.children.dispensadorDeTicketsCaracol.url}> 
-        <h2 className={style.products__title}>Dispensador de rollos de turnos</h2>
-      </Link>
+      <h2 className={style.products__title}>Dispensador de rollos de turnos</h2>
       <div className={style.products__container}>
-        <Link href={routes.landings.dispensadorDeTickets.children.dispensadorDeTicketsCaracol.url}> 
-          <Image
-            width={589}
-            height={352}
-            src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738424725/dispensador_2x-8_dwru2g.webp"
-            alt="Colores de tickets y dispensador de turnos rojo"
-          />
-        </Link>
+        <Image
+          width={1594}
+          height={952}
+          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738424725/dispensador_2x-8_dwru2g"
+          alt="Colores de tickets y dispensador de turnos rojo"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 50vw"
+        />
         <Link
           className={style.card__btn}
           href={routes.landings.dispensadorDeTickets.children.dispensadorDeTicketsCaracol.url}
@@ -25,18 +23,9 @@ function DispensadorTiquetesProductsSection() {
           Ver más
         </Link>
       </div>
-      <input
-        type="checkbox"
-        id="toggleText"
-        className={style.toggle__checkbox}
-      />
-      <p>
-        Elimina las filas y agiliza el flujo de atención, nuestro dispensador de
-        turnos ayuda a reducir el desorden y mejora la experiencia de espera,
-        asegurando que cada cliente sea atendido en el momento adecuado y sin
-        confusiones.
-      </p>
-      <label htmlFor="toggleText" className={style.toggle__label}></label>
+      <ToggleTextContent>
+        Elimina las filas y agiliza el flujo de atención, nuestro dispensador de turnos ayuda a reducir el desorden y mejora la experiencia de espera,asegurando que cada cliente sea atendido en el momento adecuado y sin confusiones.
+      </ToggleTextContent>
     </section>
   );
 }
