4086af17b417427a00f6ae0fc6f1c1cbf6c259c1
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-09 23:35:06 -0300

    componente benefits - 3

--- src/sections/rollos-de-papel-termico/landing/components/TechnicalDetails/TechnicalDetails.jsx
+++ src/sections/rollos-de-papel-termico/landing/components/TechnicalDetails/TechnicalDetails.jsx
@@ -1,106 +1,233 @@
+// import Image from "next/image";
+// import style from "./TechnicalDetails.module.scss";
+// import Link from "next/link";
+// import { routes } from "@/config/routes";
+
+// function TechnicalDetails() {
+//   return (
+//     <section className={style.technical__details}>
+//       <div>
+//         <h2 className={style.title__mobile}>Ficha Técnica</h2>
+//         <p className={style.subtitle__mobile}>Rollos térmicos</p>
+//       </div>
+//       <div className={style.technical__carousel__container}>
+//         <img
+//           src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349924/w95mg3v5l3rz4s634bso_dqg6eq.webp"
+//           alt="rollos y etiquetas para impresoras termicas"
+//           loading="lazy"
+//         />
+//       </div>
+//       <div className={style.TechnicalDetails__container}>
+//         <div className={style.technical__textContainer}>
+//           <h2>Ficha Técnica</h2>
+//           <p>Rollos térmicos</p>
+//         </div>
+//         <div className={style.technical__info}>
+//           <h3>Color - Blanco</h3>
+//           <div className={style.technical__circle__figure}></div>
+//         </div>
+//         <div className={style.technical__info}>
+//           <h3>Características</h3>
+//           <ul>
+//             <li>
+//               <div className={style.technical__circle__figure}></div>
+//               <p>
+//                 Color
+//                 <br />
+//                 Blanco
+//               </p>
+//             </li>
+//             <li>
+//               <Image
+//                 width={60}
+//                 height={60}
+//                 src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349924/uq6mantgalzbg1cwabg2_lq36yt.webp"
+//                 alt="icono de papel térmico"
+//               />
+//               <p>
+//                 Material:
+//                 <br />
+//                 Papel térmico
+//               </p>
+//             </li>
+//             <li>
+//               <Image
+//                 width={60}
+//                 height={60}
+//                 src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349922/mfucx7jyhfttrhehhv6f_gparqv.webp"
+//                 alt="icono de Rollos térmicos"
+//               />
+//               <p>
+//                 Distintos
+//                 <br />
+//                 tamaños
+//               </p>
+//             </li>
+//             <li>
+//               <Image
+//                 width={60}
+//                 height={60}
+//                 src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349922/mklyxhubukpeobmbjxso_swt54w.webp"
+//                 alt="icono de rollos termicos impresora"
+//               />
+//               <p>
+//                 Tipo de uso: para
+//                 <br />
+//                 impresora térmica
+//               </p>
+//             </li>
+//             <li>
+//               <Image
+//                 width={60}
+//                 height={60}
+//                 src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349920/jacnophczhcmtv5jmrmt_vsmuoe.webp"
+//                 alt="icono de símbolo de aprobación"
+//               />
+//               <p>
+//                 Compatible con la mayoría
+//                 <br />
+//                 de impresoras térmicas.
+//               </p>
+//             </li>
+//           </ul>
+//         </div>
+
+//         <Link
+//           className={style.technical__details__btn}
+//           href={routes.landings.rollosDePapelTermico.children.rollosTermicos.url}
+//         >
+//           Ver más
+//         </Link>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default TechnicalDetails;
+
+"use client";
+
 import Image from "next/image";
+import { useCallback, useState } from "react";
 import style from "./TechnicalDetails.module.scss";
 import Link from "next/link";
 import { routes } from "@/config/routes";
 
-function TechnicalDetails() {
+const colors = [
+  {
+    id: "blanco",
+    label: "Blanco",
+    src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349924/w95mg3v5l3rz4s634bso_dqg6eq.webp",
+    alt: "rollos y etiquetas para impresoras termicas",
+    width: 1789,
+    height: 1415,
+  },
+];
+
+const features = [
+  {
+    label: "Color: <br />Blanco",
+    figure: true,
+  },
+  {
+    label: "Material: <br />papel térmico",
+    src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349924/uq6mantgalzbg1cwabg2_lq36yt",
+    alt: "icono de ticket gris",
+    width: 136,
+    height: 128,
+  },
+  {
+    label: "Distintos<br />tamaños", 
+    src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349922/mfucx7jyhfttrhehhv6f_gparqv",
+    alt: "icono de Rollos térmicos",
+    width: 153,
+    height: 115,
+  },
+  { 
+    label: "Tipo de uso:<br /> paraimpresora térmica",
+    src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349922/mklyxhubukpeobmbjxso_swt54w",
+    alt: "icono de rollos termicos impresora",
+    width: 143,
+    height: 145,
+  },
+  {
+    label: "Compatible con la mayoría <br /> de impresoras térmicas.",
+    src: "https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349920/jacnophczhcmtv5jmrmt_vsmuoe",
+    alt: "icono de símbolo de aprobación",
+    width: 108,
+    height: 104,
+  },
+];
+
+function TurnosTechnicalDetails() {
+  const [selectedColor, setSelectedColor] = useState("blanco");
+
+  const handleColorClick = useCallback((colorId) => {
+    setSelectedColor(colorId);
+  }, [setSelectedColor]);
+
   return (
     <section className={style.technical__details}>
-      <div>
-        <h2 className={style.title__mobile}>Ficha Técnica</h2>
-        <p className={style.subtitle__mobile}>Rollos térmicos</p>
+      <div className={style.header}>
+        <h2 className={style.title}>Ficha Técnica</h2>
+        <p className={style.description}>Rollos térmicos</p>
       </div>
-      <div className={style.technical__carousel__container}>
-        <img
-          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349924/w95mg3v5l3rz4s634bso_dqg6eq.webp"
-          alt="rollos y etiquetas para impresoras termicas"
-          loading="lazy"
-        />
-      </div>
-      <div className={style.TechnicalDetails__container}>
-        <div className={style.technical__textContainer}>
-          <h2>Ficha Técnica</h2>
-          <p>Rollos térmicos</p>
+      <div className={style.carousel_container}>
+        <div className={style.carousel}>
+          {colors.map((color) => (
+            <div className={`${style.item} ${selectedColor === color.id ? style.selected : ""}`} key={color.id}> 
+              <Image 
+                src={color.src} 
+                alt={color.alt} 
+                width={color.width} 
+                height={color.height}
+                sizes="(max-width: 768px) 100vw, 80vw"
+                onClick={() => handleColorClick(color.id)}
+                className={style.image}
+              />
+            </div>
+          ))}
         </div>
-        <div className={style.technical__info}>
-          <h3>Color - Blanco</h3>
-          <div className={style.technical__circle__figure}></div>
-        </div>
-        <div className={style.technical__info}>
-          <h3>Características</h3>
+      </div> 
+      <div className={style.info_container}>
+        <div className={style.colors}>
+          <h3>Color - {colors.find((color) => color.id === selectedColor)?.label}</h3>
           <ul>
-            <li>
-              <div className={style.technical__circle__figure}></div>
-              <p>
-                Color
-                <br />
-                Blanco
-              </p>
-            </li>
-            <li>
-              <Image
-                width={60}
-                height={60}
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349924/uq6mantgalzbg1cwabg2_lq36yt.webp"
-                alt="icono de papel térmico"
+            {colors.map((color) => (
+              <li 
+                key={color.id}
+                className={`${style.color} ${style["color__" + color.id]} ${selectedColor === color.id ? style.selected : ""}`} 
+                onClick={() => handleColorClick(color.id)}
               />
-              <p>
-                Material:
-                <br />
-                Papel térmico
-              </p>
-            </li>
-            <li>
-              <Image
-                width={60}
-                height={60}
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349922/mfucx7jyhfttrhehhv6f_gparqv.webp"
-                alt="icono de Rollos térmicos"
-              />
-              <p>
-                Distintos
-                <br />
-                tamaños
-              </p>
-            </li>
-            <li>
-              <Image
-                width={60}
-                height={60}
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349922/mklyxhubukpeobmbjxso_swt54w.webp"
-                alt="icono de rollos termicos impresora"
-              />
-              <p>
-                Tipo de uso: para
-                <br />
-                impresora térmica
-              </p>
-            </li>
-            <li>
-              <Image
-                width={60}
-                height={60}
-                src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738349920/jacnophczhcmtv5jmrmt_vsmuoe.webp"
-                alt="icono de símbolo de aprobación"
-              />
-              <p>
-                Compatible con la mayoría
-                <br />
-                de impresoras térmicas.
-              </p>
-            </li>
+            ))}
           </ul>
         </div>
-
-        <Link
-          className={style.technical__details__btn}
-          href={routes.landings.rollosDePapelTermico.children.rollosTermicos.url}
-        >
-          Ver más
-        </Link>
+        <div className={style.features}>
+          <h3>Características</h3>
+          <ul>
+            {features.map((feature, index) => (
+              <li key={index}>
+                {feature.figure ? (
+                  <div className={style.figure} />
+                ) : (
+                  <Image 
+                    src={feature.src} 
+                    alt={feature.alt} 
+                    width={feature.width} 
+                    height={feature.height} 
+                    className={style.icon}
+                    sizes="(max-width: 768px) 25vw, 10vw"
+                  />
+                )}
+                <p dangerouslySetInnerHTML={{ __html: feature.label }} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Link href={routes.landings.rollosDePapelTermico.children.rollosTermicos.url} className={style.button}>Ver más</Link>
       </div>
     </section>
   );
 }
 
-export default TechnicalDetails;
+export default TurnosTechnicalDetails;
