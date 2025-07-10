8d0e4d13f447ace62da5684e5c7afd7474ace074
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-10 12:36:03 -0300

    Encuesta virtual landing - Avances 1

--- src/sections/encuesta-virtual-opinamaster/landing/components/ProductsSection/EncuestaVirtualProductsSection.jsx
+++ src/sections/encuesta-virtual-opinamaster/landing/components/ProductsSection/EncuestaVirtualProductsSection.jsx
@@ -1,26 +1,27 @@
+import ToggleTextContent from "@/components/toggle-text-content";
 import style from "./EncuestaVirtualProductsSection.module.scss";
+import Image from "next/image";
 
 function EncuestaVirtualProductsSection() {
   return (
     <section className={style.products__section}>
       <div className={style.products__container}>
-        <img
-          loading="lazy"
-          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738681738/Group_3_1_kawcef.webp"
+        <Image
+          width={660}
+          height={593}
+          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738681738/Group_3_1_kawcef"
           alt="Computador portátil que muestra un aviso emergente de la aplicación de encuestas de satisfacción"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 20vw"
         />
-        <img
-          loading="lazy"
-          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738681739/Group_4_1_h4asog.webp"
+        <Image
+          width={559}
+          height={589}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 60vw"
+          src="https://res.cloudinary.com/ddqh0mkx9/image/upload/v1738681739/Group_4_1_h4asog"
           alt="Ejemplo de una alerta en tiempo real recibida en un teléfono celular"
         />
       </div>
-      <input
-        type="checkbox"
-        id="toggleText"
-        className={style.toggle__checkbox}
-      />
-      <p>
+      <ToggleTextContent>
         Cada empresa es única, y OpinaMaster Virtual se adapta a sus
         necesidades. Este sistema de encuestas de satisfacción del cliente
         permite personalizar preguntas, elegir diferentes formatos de respuesta
@@ -30,8 +31,7 @@ function EncuestaVirtualProductsSection() {
         desde retail y restaurantes hasta clínicas y corporaciones. Con
         OpinaMaster, medir la experiencia del cliente te ayuda a tomar mejores
         decisiones.
-      </p>
-      <label htmlFor="toggleText" className={style.toggle__label}></label>
+      </ToggleTextContent>
     </section>
   );
 }
