76bd83c85d84dafb3d7ae8c6a6c9dfdbf7bf3e37
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-15 13:38:13 -0300

    avances

--- src/app/test-migration/page.jsx
+++ src/app/test-migration/page.jsx
@@ -2,11 +2,7 @@ import ProductGalleryNew from '@/sections/test-migration-nueva-galeria/ProductGa
 
 const TestMigration = () => {
   return (
-    <div style={{ fontFamily: 'Arial, sans-serif' }}>
-      <div style={{ minHeight: '800px' }}>
-        <ProductGalleryNew />
-      </div>
-    </div>
+    <ProductGalleryNew />
   );
 };
 
