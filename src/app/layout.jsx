5b57d2f3dc3db46a8dbcfe918a0fffe2c00f243a
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-23 15:04:29 -0300

    Se cambio el favicon - 2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -66,8 +66,8 @@ export default function RootLayout({ children }) {
 
         {/* Favicon explicit links */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_32_32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_16_16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
         {/* Core meta tags */}
