30ca98fd75673eedf84bd2ff5daac69ebf7a6a11
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-07-29 21:19:18 -0300

    corregimos trailling slashes del sitio

--- src/lib/html-reader.js
+++ src/lib/html-reader.js
@@ -2,26 +2,26 @@ import fs from 'fs';
 import path from 'path';
 import { enlacesParaReemplazar } from '@/config/enlaces-para-reemplazar-en-html';
 
-const replaceLinksInHtml = (htmlContent) => {
-  let modifiedHtml = htmlContent;
+// const replaceLinksInHtml = (htmlContent) => {
+//   let modifiedHtml = htmlContent;
 
-  // Recorremos la lista de enlaces a reemplazar
-  enlacesParaReemplazar.forEach(({ previous, current }) => {
-    // Si el enlace 'previous' es una URL completa, necesitamos reemplazar solo el path.
-    // Si el enlace 'previous' contiene el dominio base, extraemos el path solo.
-    const pathToReplace = new URL(previous).pathname;
+//   // Recorremos la lista de enlaces a reemplazar
+//   enlacesParaReemplazar.forEach(({ previous, current }) => {
+//     // Si el enlace 'previous' es una URL completa, necesitamos reemplazar solo el path.
+//     // Si el enlace 'previous' contiene el dominio base, extraemos el path solo.
+//     const pathToReplace = new URL(previous).pathname;
     
-    // Creamos una expresión regular para encontrar todas las instancias del `path` en el contenido HTML
-    const regex = new RegExp(pathToReplace, 'g');
-    const regexUrlBase = new RegExp('https://tecnologiaplus.com/', 'g');
+//     // Creamos una expresión regular para encontrar todas las instancias del `path` en el contenido HTML
+//     const regex = new RegExp(pathToReplace, 'g');
+//     const regexUrlBase = new RegExp('https://tecnologiaplus.com/', 'g');
     
-    // Reemplazamos el `path` encontrado por la URL actual
-    modifiedHtml = modifiedHtml.replace(regex, current);
-    modifiedHtml = modifiedHtml.replace(regexUrlBase, '/');
-  });
+//     // Reemplazamos el `path` encontrado por la URL actual
+//     modifiedHtml = modifiedHtml.replace(regex, current);
+//     modifiedHtml = modifiedHtml.replace(regexUrlBase, '/');
+//   });
 
-  return modifiedHtml;
-};
+//   return modifiedHtml;
+// };
 
 export const htmlReader = (filePath) => {
   const absolutePath = path.join(process.cwd(), 'src', 'data', 'blogs', 'html', 'articles', filePath);
@@ -32,6 +32,7 @@ export const htmlReader = (filePath) => {
   
   const htmlContent = fs.readFileSync(absolutePath, 'utf-8');
 
-  const modifiedHtml = replaceLinksInHtml(htmlContent);
-  return modifiedHtml;
+  // const modifiedHtml = replaceLinksInHtml(htmlContent);
+  // return modifiedHtml;
+  return htmlContent;
 }
