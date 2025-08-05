37ae89a129792907a5467f16f7354fbf13ecf23f
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-08-05 16:13:22 -0300

    Imagenes nuevas de seccion productos en Home

--- src/sections/home/ProductsSection/HomeProductsSection.jsx
+++ src/sections/home/ProductsSection/HomeProductsSection.jsx
@@ -3,88 +3,89 @@ import { routes } from "@/config/routes";
 import LinkCard from "@/components/ui/link";
 import Image from "next/image";
 import Link from "next/link";
+import { getOptimizedImageUrl } from "@/components/galeria-ver-mas/utils/imageUtils";
 
 const products = [
   {
     title: "TurnoExpress",
     description: "Rapidez y agilidad para tus filas.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1738350028/TurnoExpress_2x-8_ri7zqh_xjz8bw",
+    imageSrc: "v1754420148/turnero_turnoexpress_bl0pdv",
     altText: "Imagen de Turnero Digital TurnoExpress - TecnologíaPlus",
     link: routes.landings.turneroTurnoexpress.url,
   }, 
   {
     title: "TurnoMaster",
     description: "Digitaliza la gestión de turnos.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1738349924/TurnoMaster_2x-8_1_t4wh3f_ooru74",
+    imageSrc: "v1754420147/sistema_de_turnos_turnomaster_nxmrcv",
     altText: "Imagen de Sistema de Turnos de Espera Turnomaster - TecnologíaPlus",
     link: routes.landings.sistemasDeTurnosTurnomaster.url,
   }, 
   {
     title: "Localizadores",
     description: "Lleva el autoservicio a otro nivel.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1741708165/NUEVA_etp9mi",
+    imageSrc: "v1754420152/localizadores_para_restaurantes_za7yhv", 
     altText: "Imagen de Localizadores para Restaurantes - TecnologíaPlus",
     link: routes.landings.localizadoresParaRestaurantes.url,
   },
   {
     title: "Llamadores de meseros",
     description: "El botón que eleva la calidad del servicio.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1743107327/NUEVA_4x-8_1_wyooar",
+    imageSrc: "v1754420144/llamadores_de_meseros_hfsfxc",
     altText: "Imagen de Llamadores de Meseros - TecnologíaPlus",
     link: routes.landings.llamadoresDeMeseros.url,
   }, 
   {
     title: "Llamado de enfermería",
     description: "Facilita la comunicación paciente - enfermera. ",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1738596111/cuidamaster_2x-8_dlawzx",
+    imageSrc: "v1754420142/llamado_de_enfermeri%CC%81a_cuidamaster_gctjus",
     altText: "Imagen de Llamado de Enfermería Cuidamaster - TecnologíaPlus",
     link: routes.landings.llamadoDeEnfermeriaCuidamaster.url,
   }, 
   {
     title: "Calificador de servicios",
     description: "Toma decisiones con la opinión de tus clientes",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1738607994/calificador_de_servicios_2x-8_pjnvol",
+    imageSrc: "v1754420542/calificador_de_servicios_opinamaster_gwnjgp",
     altText: "Imagen de Calificador de Servicio OpinaMaster - TecnologíaPlus",
     link: routes.landings.calificadorDeServicioAlClienteOpinamaster.url,
   }, 
   {
     title: "OpinaMaster",
     description: "Transforma opiniones en acciones estratégicas.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1738685536/Opinamaster_2x-8_sg1wyy",
+    imageSrc: "v1754420143/encuesta_virtual_opinamaster_utrsch",
     altText: "Imagen de Encuesta Virtual OpinaMaster - TecnologíaPlus",
     link: routes.landings.encuestaVirtual.url,
   }, 
   {
     title: "Dispensador de tiquetes",
     description: "Complemento práctico para tomar el turno.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1738349966/dispensador_de_tiquetes_2x-8_qusuqr_fuxs8k",
+    imageSrc: "v1754420143/dispensador_de_tiquetes_turnoexpress_r5nx3b",
     altText: "Imagen de Dispensador de Tickets TurnoExpress - TecnologíaPlus",
     link: routes.landings.dispensadorDeTickets.url,
   }, 
   {
     title: "Rollos de turnos",
     description: "Organiza y mejora el servicio.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1745247230/Frame_1_35_vku1gs",
+    imageSrc: "v1754420149/rollos_de_fichos_para_turnos_turnoexpress_scqobh",
     altText: "Imagen de Rollos de Turnos TurnoExpress - TecnologíaPlus",
     link: routes.landings.rollosDeFichosParaTurnos.url,
   }, 
   {
     title: "Rollos térmicos",
     description: "Perfectos para tickets, facturas y más.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1738349920/image-1_1_ljyxis_qdsmdq",
+    imageSrc: "v1754420146/rollos_de_papel_termico_jnusbb",
     altText: "Imagen de Rollos de Papel Térmico - TecnologíaPlus",
     link: routes.landings.rollosDePapelTermico.url,
   },
   {
     title: "Desarrollo de software",
     description: "Ajustado a tus necesidades.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1738945022/11_2x-8_lb72ht",
+    imageSrc: "v1754420729/desarrollo_de_software_yp7see",
     altText: "Imagen de Desarrollo de Software - TecnologíaPlus",
   }, 
   {
     title: "Soporte Técnico",
     description: "Maximiza la vida útil y el rendimiento de tu inversión.",
-    imageSrc: "https://res.cloudinary.com/ddqh0mkx9/image/upload/c_scale,f_webp,w_440/v1738945024/12_2x-8_nnlbla",
+    imageSrc: "v1754420730/soporte_tecnico_omhpvx",
     altText: "Imagen de Soporte Técnico - TecnologíaPlus",
   }
 ]
@@ -95,7 +96,7 @@ function CardContent({ product }) {
       <h3 className={style.productsSection__cardTitle}>{product.title}</h3>
       <p className={style.productsSection__cardDescription}>{product.description}</p>
       <Image
-        src={product.imageSrc}
+        src={getOptimizedImageUrl({ url: product.imageSrc, width: 600, quality: 80 })}
         alt={product.altText}
         width={436}
         height={350}
